be38278c547c6322eee89164916f53fe088023f9
Dmytro Kostochko <alerion.um@gmail.com>
2025-05-24 16:16:16 +0200

    Code cleanup

--- anki_training/translate_command.py
+++ anki_training/translate_command.py
@@ -4,16 +4,12 @@ from anki_training.config import settings
 
 
 def translate_command():
-    client = OpenAI(
-        api_key=settings.OPEN_AI_API_KEY
-    )
+    client = OpenAI(api_key=settings.OPEN_AI_API_KEY)
 
     completion = client.chat.completions.create(
         model="gpt-4o-mini",
         store=True,
-        messages=[
-            {"role": "user", "content": "write a haiku about ai"}
-        ]
+        messages=[{"role": "user", "content": "write a haiku about ai"}],
     )
 
-    print(completion.choices[0].message);
+    print(completion.choices[0].message)
